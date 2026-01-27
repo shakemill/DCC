b641a2c7350a23185a239180bb418c4620e5d74d
Henri-Mill Fetchouang <shakemill@MacBook-Pro-de-Henri-Mill.local>
2026-01-27 16:39:42 +0100

    chore: ignore archives, Lenis 0.6, dev:webpack

--- components/Lenis.jsx
+++ components/Lenis.jsx
@@ -6,7 +6,7 @@ import Lenis from 'lenis';
 export default function LenisScroll() {
     useEffect(() => {
         const lenis = new Lenis({
-            duration: 1.2,
+            duration: 0.6,
             smoothWheel: true,
             smoothTouch: false,
             anchors: true,
